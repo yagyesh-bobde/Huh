8f5a5a0215c4e453f4c72a5827e0fe89605c715c
Yagyesh Bobde <yagyeshSRbobde@gmail.com>
2024-02-10 00:51:26 +0530

    removed db connect callback

--- server/db.js
+++ server/db.js
@@ -3,10 +3,14 @@ require('dotenv').config()
 
 const mongouri = process.env.DB_URI || "mongodb+srv://yagyesh:yagyesh@developers-zone.69fu5rm.mongodb.net" 
 
-const connectToMongo = () =>{
-    mongoose.connect(mongouri,()=>{
-        console.log("Connected to Mongo Successfully")
-    })
+const connectToMongo = async() =>{
+    try {
+        mongoose.set("strictQuery", false)
+        mongoose.connect(mongouri)
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
 }
 
 
